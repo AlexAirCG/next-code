df12028e11aa8daf08b362eafc60e3ae5263aca7
AlexAirCG <alexairggg@yandex.ru>
2025-03-06 19:48:55 +0300

    chor: add prettier and shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">hello</div>;
+  return (
+    <div className="">
+      <Button className="border">hello</Button>
+      <div>hello</div>
+    </div>
+  );
 }
