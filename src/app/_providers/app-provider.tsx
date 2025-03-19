6ef61d7a37a39d3ed1bbd01d292b3e313c8fdaee
AlexAirCG <alexairggg@yandex.ru>
2025-03-19 18:41:27 +0300

    18:41

--- src/app/_providers/app-provider.tsx
+++ src/app/_providers/app-provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { AppSessionProvider } from "@/entities/session/app-session-provider";
 import { ComposeChildren } from "@/lib/react";
 import React from "react";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  return <ComposeChildren>{children}</ComposeChildren>;
+  return (
+    <ComposeChildren>
+      <AppSessionProvider />
+      {children}
+    </ComposeChildren>
+  );
 }
