c1d2bed86c7e4c70c9c2bfb1dae83982b416c076
AlexAirCG <alexairggg@yandex.ru>
2025-03-20 11:06:43 +0300

    20.03.25-11:05 add base auth

--- src/app/_providers/app-provider.tsx
+++ src/app/_providers/app-provider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { queryClient } from "@/components/api/query-client";
 import { AppSessionProvider } from "@/entities/session/app-session-provider";
 import { ComposeChildren } from "@/lib/react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <ComposeChildren>
       <AppSessionProvider />
+      <QueryClientProvider client={queryClient} />
       {children}
     </ComposeChildren>
   );
--- src/widgets/app-header/ui/profile.tsx
+++ src/widgets/app-header/ui/profile.tsx
@@ -11,14 +11,30 @@ import {
 import { LogOut, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAppSession } from "@/entities/session/use-app-session";
+import { useSignOut } from "@/features/auth/use-sign-out";
+import { Skeleton } from "@/components/ui/skeleton";
+import { SignInButton } from "@/features/auth/sign-in-button";
 
 export function Profile() {
+  const session = useAppSession();
+  const { signOut, isPending: isLoadingSignOut } = useSignOut();
+
+  if (session.status === "loading") {
+    return <Skeleton className="w-8 h-8 rounded-full" />;
+  }
+
+  if (session.status === "unauthenticated") {
+    return <SignInButton />;
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="p-px rounded-full self-center h-8 w-8">
           <Avatar className="w-8 h-8 border hover:border-2">
+            <AvatarImage src={session.data?.user.image} />
             <AvatarFallback>AC</AvatarFallback>
           </Avatar>
         </Button>
@@ -27,7 +43,7 @@ export function Profile() {
         <DropdownMenuLabel>
           <p>Мой аккаунт</p>
           <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
-            Paromov
+            {session.data?.user.name}
           </p>
         </DropdownMenuLabel>
         <DropdownMenuGroup></DropdownMenuGroup>
@@ -39,7 +55,10 @@ export function Profile() {
               <span>Профиль</span>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={isLoadingSignOut}
+            onClick={() => signOut()}
+          >
             <LogOut className="mr-2 h-4 w-4" />
             <span>Выход</span>
           </DropdownMenuItem>
