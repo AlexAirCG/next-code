e8fbfd41caae8f0875c2a09c839835ec4622fd45
AlexAirCG <alexairggg@yandex.ru>
2025-03-22 16:45:56 +0300

    16:45

--- src/config/private.ts
+++ src/config/private.ts
@@ -1,14 +1,14 @@
-// import { z } from "zod";
+import { z } from "zod";
 
-// const privateConfigSchema = z.object({
-//   GOOGLE_CLIENT_ID: z.string().optional(),
-//   GOOGLE_CLIENT_SECRET: z.string().optional(),
+const privateConfigSchema = z.object({
+  GOOGLE_CLIENT_ID: z.string().optional(),
+  GOOGLE_CLIENT_SECRET: z.string().optional(),
 
-//   EMAIL_SERVER_USER: z.string(),
-//   EMAIL_SERVER_PASSWORD: z.string(),
-//   EMAIL_SERVER_HOST: z.string(),
-//   EMAIL_SERVER_PORT: z.number(),
-//   EMAIL_FROM: z.string(),
-// });
+  EMAIL_SERVER_USER: z.string(),
+  EMAIL_SERVER_PASSWORD: z.string(),
+  EMAIL_SERVER_HOST: z.string(),
+  EMAIL_SERVER_PORT: z.string(),
+  EMAIL_FROM: z.string(),
+});
 
-// export const privateConfig = privateConfigSchema.parse(process.env);
+export const privateConfig = privateConfigSchema.parse(process.env);
--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -4,27 +4,27 @@ import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { dbClient } from "@/lib/db";
 import { compact } from "lodash-es";
-// import { privateConfig } from "@/config/private";
+import { privateConfig } from "@/config/private";
 
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
   providers: compact([
     EmailProvider({
       server: {
-        host: process.env.EMAIL_SERVER_HOST,
+        host: privateConfig.EMAIL_SERVER_HOST,
         // port: process.env.EMAIL_SERVER_PORT,
         auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
         },
       },
       from: process.env.EMAIL_FROM,
     }),
-    // privateConfig.GOOGLE_CLIENT_ID &&
-    //   privateConfig.GOOGLE_CLIENT_SECRET &&
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
+    privateConfig.GOOGLE_CLIENT_ID &&
+      privateConfig.GOOGLE_CLIENT_SECRET &&
+      GoogleProvider({
+        clientId: privateConfig.GOOGLE_CLIENT_ID,
+        clientSecret: privateConfig.GOOGLE_CLIENT_SECRET,
+      }),
   ]),
 };
