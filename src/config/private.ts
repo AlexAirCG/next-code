e6722b0716a3849b3fb8329d0f1b423c32a77e5e
AlexAirCG <alexairggg@yandex.ru>
2025-03-22 16:21:55 +0300

    22.03.25-16:21

--- src/config/private.ts
+++ src/config/private.ts
@@ -7,7 +7,7 @@ const privateConfigSchema = z.object({
   EMAIL_SERVER_USER: z.string(),
   EMAIL_SERVER_PASSWORD: z.string(),
   EMAIL_SERVER_HOST: z.string(),
-  EMAIL_SERVER_PORT: z.any(),
+  EMAIL_SERVER_PORT: z.number(),
   EMAIL_FROM: z.string(),
 });
 
